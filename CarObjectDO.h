b3c4aac47367212075051e2a1c8f3c1798469782
Maxian Nicu <maxiannicu@gmail.com>
2016-04-18 21:55:44 +0300

    Added Output Car objects

---
+++ CarObjectDO.h
@@ -0,0 +1,106 @@
+#pragma once
+#include "Sprite.h"
+#include <bemapiset.h>
+#include <wincon.h>
+
+class CarObjectDO : public Sprite
+{
+private:
+	int vX;
+	int vY;
+	bool leftLight = false;
+	bool rightLight = false;
+public:
+
+	CarObjectDO(HINSTANCE hInst, LPCWSTR path) : Sprite(hInst, path, 580, 520, 70, 50) { }
+
+	void virtual render(HWND hwnd, PAINTSTRUCT ps, HDC hdc) {
+		BITMAP 			bitmap;
+		HDC 			hdcMem;
+		HGDIOBJ 		oldBitmap;
+
+		hdcMem = CreateCompatibleDC(hdc);
+		oldBitmap = SelectObject(hdcMem, hBitmap);
+
+		GetObject(hBitmap, sizeof(bitmap), &bitmap);
+		BitBlt(hdc, x - width / 2, y - height / 2, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
+		if (leftLight)
+			LeftLight(hdc);
+		if (rightLight)
+			RightLight(hdc);
+
+		SelectObject(hdcMem, oldBitmap);
+		DeleteDC(hdcMem);
+	}
+
+	void turnLeft() {
+		rightLight = false;
+		leftLight = true;
+	}
+
+	void turnRight() {
+		rightLight = true;
+		leftLight = false;
+	}
+
+	void switchOffLights() {
+		leftLight = rightLight = false;
+	}
+
+	void LeftLight(HDC hdc) {
+		SelectObject(hdc, CreateSolidBrush(RGB(255, 165, 0)));
+		int size = 5;
+
+		int left = x - width / 2 - size;
+		int top = y - width / 2 + size;
+		int right = x - height / 2 - size;
+		int bottom = y - height / 2 + size;
+
+		Ellipse(hdc, left, top, right, bottom);
+
+
+
+		left = x + width / 2 + size;
+		top = y - width / 2 + size;
+		right = x + height / 2 + size;
+		bottom = y - height / 2 + size;
+
+		Ellipse(hdc, left, top, right, bottom);
+
+
+	}
+
+	void RightLight(HDC hdc) {
+		SelectObject(hdc, CreateSolidBrush(RGB(255, 165, 0)));
+		int size = 5;
+		int left = x + width / 2 + size;
+		int top = y + width / 2 - size;
+		int right = x + height / 2 + size;
+		int bottom = y + height / 2 - size;
+
+		Ellipse(hdc, left, top, right, bottom);
+
+		left = x - width / 2 - size;
+		top = y + width / 2 - size;
+		right = x - height / 2 - size;
+		bottom = y + height / 2 - size;
+
+		Ellipse(hdc, left, top, right, bottom);
+	}
+
+	void setVelocity(int x, int y) {
+		this->vX = x;
+		this->vY = y;
+	}
+
+	virtual void update() override {
+		x += vX;
+		y += vY;
+	}
+
+	void go() {
+		this->setVelocity(-5, 0);
+	}
+
+};
+
